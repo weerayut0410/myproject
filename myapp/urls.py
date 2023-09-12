ec6d0705a8d44a4a78dd8f9771cdc1e08d914dcc
weerayut0410 <weerayut.la.65@ubu.ac.th>
2023-09-12 13:22:16 +0700

    sss

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,8 @@ from myapp import views
 urlpatterns = [
     path('',views.Index ),
     path('hello/',views.hello ),
-    path('db/',views.db)
+    path('db/',views.db),
+    path('add/',views.add),
+    path('update/<product_id>',views.update),
+    path('delete/<product_id>',views.delete),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,18 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from myapp.models import Product
+from django.contrib import messages
+
 
 # Create your views here.
+
+def delete(request, product_id):
+    product = Product.objects.get(id=product_id)
+    product.delete()
+    messages.success(request,'Data deleted sucessfully')
+    return redirect('/db')
+
+
+def update(request, product_id):
+    if request.method == "POST":
+        product = Product.objects.get(id=product_id)
+        product.name = request.POST['name']
+        product.price = request.POST['price']
+        product.save()
+        messages.success(request,'Data updated sucessfully')
+        return redirect('/db')
+    product = Product.objects.get(id=product_id)
+    return render(request,"form_update.html",{"product":product})
+
+def add(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        price = request.POST['price']
+
+        product = Product.objects.create(name=name,price=price)
+        product.save()
+        messages.success(request,'Data added sucessfully')
+        return redirect("/db")
+
+    return render(request, "form_add.html")
+
 def db(request):
     all_product = Product.objects.all
     some_product = Product.objects.filter(price__gt=5000)
-    return render(request, 'db.html',{"all_product":all_product,'some_product':some_product})
+    return render(
+        request, "db.html", {"all_product": all_product, "some_product": some_product}
+    )
+
 
 def Index(request):
-    name = 'John Doe'
-    age = 42
-    return render(request, 'index.html',{"name":name,"age":age})
+    name = "weerayut laksin"
+    age = 20
+    return render(request, "index.html", {"name": name, "age": age})
+
 
 def hello(request):
     return HttpResponse("<h1>Hello</h1>")
-
